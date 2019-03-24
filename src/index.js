661a8feff62f2123b6beb3e7ce7b31c51d4bff83
Animob <anngwork@gmail.com>
2019-03-24 23:43:30 +0300

    feat: add brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bracketsStack = [];
+  var bracketsBalance = true;
+
+  if (str.length%2 != 0) {
+    return false;
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    var bracket = str[i];
+
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (bracket == bracketsConfig[j][0]) {
+        bracketsStack.push(bracketsConfig[j][1]);
+
+        break;
+      } else if (bracket == bracketsConfig[j][1]) {
+        var bracketLast = bracketsStack.pop();
+
+        if (bracketLast != bracketsConfig[j][1]) {
+          bracketsBalance = false;
+        }
+
+        break;
+      }
+
+
+    }
+
+    if (bracketsBalance == false) {
+      return false;
+    }
+  }
+
+  if (bracketsStack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+
 }
